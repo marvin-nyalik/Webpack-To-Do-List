f110f004cd587f0a5403c5d31412ca1a69cf060d
MARVIN <o.silverygold@gmail.com>
2023-06-06 15:01:14 +0300

    Update the list items in dynamic function

--- src/index.js
+++ src/index.js
@@ -1,15 +1,45 @@
-import _ from 'lodash';
 import './style.css';
 
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
-  
+const list = () => {
+  const navList = document.getElementById('list');
+  const myList = [
+    {
+      description: 'Take out trash',
+      completed: false,
+      index: 1,
+    },
+    {
+      description: 'Make breakfast',
+      completed: true,
+      index: 2,
+    },
+    {
+      description: 'Code CSS',
+      completed: false,
+      index: 3,
+    },
+    {
+      description: 'Learn React',
+      completed: true,
+      index: 4,
+    },
+  ];
+
+  myList.forEach((item) => {
+    const listItem = `
+             <li>
+                <div class="to-do-item">
+                    <input type="checkbox" id="demoCheckbox" name="checkbox" value="1">
+                    <p>${item.description}</p>
+                </div>
+                <i class='bx bx-dots-vertical-rounded to-do-more'> </i>
+            </li>
+    `;
+
+    navList.innerHTML += listItem;
+  });
+};
+
+(list());
+
+export default list;
