9dc784c92351dc727bbd83aa3e1c567d7fe7f68e
MARVIN <o.silverygold@gmail.com>
2023-06-07 22:52:54 +0300

    Fix eslint errors

--- src/listUtils.js
+++ src/listUtils.js
@@ -61,9 +61,9 @@ export const handleCheckboxChange = (event) => {
   const checkbox = event.target;
   const listItem = checkbox.closest('li');
   const key = listItem.querySelector('p').innerHTML;
-  
-  for(let i = 0; i < myList.length; i += 1){
-    if (myList[i].description === key){
+
+  for (let i = 0; i < myList.length; i += 1) {
+    if (myList[i].description === key) {
       myList[i].completed = true;
     }
   }
@@ -140,7 +140,7 @@ export const list = () => {
 export const clearCompleted = () => {
   myList = JSON.parse(localStorage.getItem('tasks'));
   const completed = myList.filter((task) => task.completed !== true);
-  for(let j = 0; j < completed.length; j += 1){
+  for (let j = 0; j < completed.length; j += 1) {
     completed[j].index = j + 1;
   }
   localStorage.setItem('tasks', JSON.stringify(completed));
